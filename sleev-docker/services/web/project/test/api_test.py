1c0d101327e37a2af2c35495bf8dc9e508c3829c
joao99lopes <joao.aclopes99@gmail.com>
2023-09-03 17:20:06 +0100

    authtoken

--- sleev-docker/services/web/project/test/api_test.py
+++ sleev-docker/services/web/project/test/api_test.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 
-url = 'http://127.0.0.1:1338/user/login'  # Replace with your Flask server URL
+url = 'http://127.0.0.1:1338/user/migrate'  # Replace with your Flask server URL
 add_user_data = {
     'first_name': 'joao', 
     'last_name': 'Lopes', 
@@ -16,12 +16,12 @@ add_user_data = {
 delete_user_data = {'email': 'alice_inwasfonderland@test.com'}
 
 login_data = {
-    'email': 'joao@test.com', 
-    'password': 'complexpassword',
+    'email': 'joao', 
+    'password': '1245',
     }
     
 #response = requests.post(url, json=add_user_data)
-response = requests.post(url, json=login_data)
+response = requests.post(url)
 
 print(response.status_code)  # Get the HTTP status code of the response
 print(response.json())
